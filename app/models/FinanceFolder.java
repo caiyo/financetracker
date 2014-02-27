691c0f92b4876f866f10ed5bb2799288c1f32da5
Kyle Wong <wonger35@gmail.com>
2014-02-26 21:49:27 -0800

    first commit

---
+++ app/models/FinanceFolder.java
@@ -0,0 +1,28 @@
+package models;
+
+import javax.persistence.*;
+import play.db.ebean.*;
+import com.avaje.ebean.*;
+
+@Entity
+public class FinanceFolder extends Model{
+
+	@Id
+	public int id;
+	public String name;
+	public int total;
+	@OneToOne
+	public User user;
+	public static Finder<Integer, FinanceFolder> find = new Finder(Integer.class, FinanceFolder.class);
+
+	public FinanceFolder (String name, User user){
+		this.name=name;
+		this.user=user;
+		this.total=0;
+	}
+
+	//public 
+
+
+
+}
